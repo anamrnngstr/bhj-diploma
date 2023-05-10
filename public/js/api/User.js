86b92402da1161c83855a89e8d2233c140faf7d6
Ana Mrnngstr <tyurinaana666@gmail.com>
2023-05-10 22:51:12 +0300

    edited API

--- public/js/api/User.js
+++ public/js/api/User.js
@@ -9,6 +9,7 @@ class User {
    * локальном хранилище.
    * */
   static setCurrent(user) {
+    localStorage.setItem('user', JSON.stringify(user));
 
   }
 
@@ -17,7 +18,7 @@ class User {
    * пользователе из локального хранилища.
    * */
   static unsetCurrent() {
-
+    localStorage.removeItem('user');
   }
 
   /**
@@ -25,7 +26,7 @@ class User {
    * из локального хранилища
    * */
   static current() {
-
+    return JSON.parse(localStorage.getItem('user'));
   }
 
   /**
@@ -33,7 +34,19 @@ class User {
    * авторизованном пользователе.
    * */
   static fetch(callback) {
-
+    createRequest({
+      url: this.URL + '/current',
+      method: 'GET',
+      data: null,
+      callback: (err, response) => {
+        if (response && response.user) {
+          this.setCurrent(response.user);
+        } else {
+          this.unsetCurrent();
+        }
+        callback(err, response);
+      }
+    });
   }
 
   /**
@@ -65,6 +78,18 @@ class User {
    * */
   static register(data, callback) {
 
+    createRequest({
+      url: this.URL + '/register',
+      method: 'POST',
+      data,
+      callback: (err, response) => {
+        if (response && response.user) {
+          this.setCurrent(response.user);
+        }
+        callback(err, response);
+      }
+    });
+
   }
 
   /**
@@ -72,6 +97,16 @@ class User {
    * выхода необходимо вызвать метод User.unsetCurrent
    * */
   static logout(callback) {
-
+    createRequest({
+      url: this.URL + '/logout',
+      method: 'POST',
+      data,
+      callback: (err, response) => {
+        if (response && response.user) {
+          this.setCurrent(response.user);
+        }
+        callback(err, response);
+      }
+    });
   }
 }
--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,36 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    if (options.method === 'GET') {
+        let strUrl = `${options.url}?`;
+        for (let key in options.data) {
+            strUrl += `${key}=${options.data[key]}&`;
+        }
+        const url = strUrl.slice(0, -1);
+
+        xhr.open(options.method, url);
+
+        xhr.send();
+    } else {
+        let formData = new FormData();
+        for (let key in options.data) {
+            formData.append(key, options.data[key]);
+        }
+
+        xhr.open(options.method, options.url);
+
+        xhr.send(formData);
+    }
+    xhr.addEventListener('readystatechange', function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            options.callback(null, xhr.response);
+        }
+
+        if (xhr.readyState === 4 && xhr.status !== 200) {
+            options.callback(xhr.response.error, xhr.response);
+        }
+    });
 
 };
